79cdaa393fa25ddc659d944308e604e31e66b539
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-03-27 16:04:44 +0200

    configure gr8 classpath to include compileOnly

--- common/src/main/kotlin/com/freeletics/gradle/setup/Gr8.kt
+++ common/src/main/kotlin/com/freeletics/gradle/setup/Gr8.kt
@@ -2,20 +2,34 @@ package com.freeletics.gradle.setup
 
 import com.gradleup.gr8.Gr8Extension
 import org.gradle.api.Project
+import org.gradle.api.attributes.Usage
+import org.gradle.api.attributes.Usage.JAVA_API
 
 fun Project.setupGr8() {
     val shadeConfiguration = configurations.create("shade")
 
-    configurations.named("compileOnly").configure {
+    // make all shaded dependencies available during compilation
+    configurations.named("compileClasspath").configure {
         it.extendsFrom(shadeConfiguration)
     }
+    // make all shaded dependencies available during tests
     configurations.named("testImplementation").configure {
         it.extendsFrom(shadeConfiguration)
+    }
+
+    // add compileOnly dependencies to r8's classpath to avoid class not found warnings
+    // needs to be an extra configuration because we want to filter it and compileOnly can't be resolved
+    val shadeClassPathConfiguration = configurations.create("shadeClassPath") {
+        it.extendsFrom(configurations.getByName("compileOnly"))
+        it.attributes { attributes ->
+            attributes.attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage::class.java, JAVA_API))
+        }
     }
 
     extensions.configure(Gr8Extension::class.java) { extension ->
         val shadowedJar = extension.create("gr8") {
             it.configuration(shadeConfiguration.name)
+            it.classPathConfiguration(shadeClassPathConfiguration.name)
             it.stripGradleApi(true)
             if (rootProject.file(FILE_NAME).exists()) {
                 it.proguardFile(rootProject.file(FILE_NAME))
