66b62fe14824da44d3129a3e8d7c22bbf13adbcd
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-04-06 15:20:43 +0200

    add some workarounds for issues

--- common/src/main/kotlin/com/freeletics/gradle/setup/Gr8.kt
+++ common/src/main/kotlin/com/freeletics/gradle/setup/Gr8.kt
@@ -4,6 +4,8 @@ import com.gradleup.gr8.Gr8Extension
 import org.gradle.api.Project
 import org.gradle.api.attributes.Usage
 import org.gradle.api.attributes.Usage.JAVA_API
+import org.gradle.api.attributes.plugin.GradlePluginApiVersion
+import org.gradle.api.internal.artifacts.dsl.dependencies.GradlePluginVariantsSupport
 
 internal fun Project.setupGr8() {
     val shadeConfiguration = configurations.create("shade")
@@ -23,7 +25,22 @@ internal fun Project.setupGr8() {
         it.extendsFrom(configurations.getByName("compileOnly"))
         it.attributes { attributes ->
             attributes.attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage::class.java, JAVA_API))
+            // TODO workaround for KGP, it ships multiple variants for separate Gradle versions and we end up having
+            //  both the one without classifier and the regular one on the classpath which causes duplicate class
+            //  errors. Ideally we'd force it to only the version that matches our Gradle version but that doesn't
+            //  seem to be possible. As a workaround we set the matching attribute to Gradle 1.0 which makes us match
+            //  only the general variant. Downside of this is that R8 will still have some missing class warnings
+            //  which need to be suppressed with `-dontwarn org.jetbrains.kotlin.**`
+            attributes.attribute(GradlePluginApiVersion.GRADLE_PLUGIN_API_VERSION_ATTRIBUTE, objects.named(GradlePluginApiVersion::class.java, "1.0"))
         }
+
+        // already added to shade
+        it.exclude(mapOf("group" to "org.jetbrains.kotlin", "module" to "kotlin-stdlib"))
+        // included in gradle-api and would cause duplicate class issues
+        it.exclude(mapOf("group" to "org.jetbrains", "module" to "annotations"))
+        // included in gradle-api and would cause duplicate class issues
+        it.exclude(mapOf("group" to "javax.inject", "module" to "javax.inject"))
+
     }
 
     extensions.configure(Gr8Extension::class.java) { extension ->
