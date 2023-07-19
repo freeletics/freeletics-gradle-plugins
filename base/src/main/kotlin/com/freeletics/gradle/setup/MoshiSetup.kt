c733ef000cd5b50c5bc98c416d77395d8a1a3f77
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-07-19 09:11:15 +0200

    apply ksp when enabling moshix

--- base/src/main/kotlin/com/freeletics/gradle/setup/MoshiSetup.kt
+++ base/src/main/kotlin/com/freeletics/gradle/setup/MoshiSetup.kt
@@ -5,6 +5,8 @@ import org.gradle.api.Project
 
 internal fun Project.configureMoshi(sealed: Boolean) {
     plugins.apply("dev.zacsweers.moshix")
+    // we're using moshi-ir but proguard rules are generated with KSP
+    plugins.apply("com.google.devtools.ksp")
 
     extensions.configure(MoshiPluginExtension::class.java) {
         it.enableSealed.set(sealed)
