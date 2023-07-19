5e59057699fbc85465946dd573b5495466e60235
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-07-19 09:26:40 +0200

    Revert "apply ksp when enabling moshix"
    This reverts commit c733ef000cd5b50c5bc98c416d77395d8a1a3f77.

--- base/src/main/kotlin/com/freeletics/gradle/setup/MoshiSetup.kt
+++ base/src/main/kotlin/com/freeletics/gradle/setup/MoshiSetup.kt
@@ -5,8 +5,6 @@ import org.gradle.api.Project
 
 internal fun Project.configureMoshi(sealed: Boolean) {
     plugins.apply("dev.zacsweers.moshix")
-    // we're using moshi-ir but proguard rules are generated with KSP
-    plugins.apply("com.google.devtools.ksp")
 
     extensions.configure(MoshiPluginExtension::class.java) {
         it.enableSealed.set(sealed)
