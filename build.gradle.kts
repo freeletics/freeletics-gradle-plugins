bdee49c2ee4238e30d068cefec026489375bbd40
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-12-04 12:24:56 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,29 +10,3 @@ plugins {
 
     alias(libs.plugins.fgp.root)
 }
-
-dependencyAnalysis {
-    issues {
-        all {
-            onUsedTransitiveDependencies {
-                exclude(":base")
-            }
-        }
-    }
-}
-
-subprojects {
-    plugins.withId("com.vanniktech.maven.publish") {
-        extensions.configure(MavenPublishBaseExtension::class.java) {
-            pom {
-                licenses {
-                    license {
-                        name.set("The Apache Software License, Version 2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                        distribution.set("repo")
-                    }
-                }
-            }
-        }
-    }
-}
