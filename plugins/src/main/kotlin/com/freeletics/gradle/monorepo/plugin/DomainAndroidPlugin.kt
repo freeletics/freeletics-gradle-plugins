4db16c96c87717c5c4c389f98c839347634fca3a
Gabriel Ittner <gabriel.ittner@gmail.com>
2024-04-03 09:34:21 +0200

    remove LegacyExtension from domain-android plugin

--- plugins/src/main/kotlin/com/freeletics/gradle/monorepo/plugin/DomainAndroidPlugin.kt
+++ plugins/src/main/kotlin/com/freeletics/gradle/monorepo/plugin/DomainAndroidPlugin.kt
@@ -16,33 +16,23 @@ public abstract class DomainAndroidPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.plugins.apply(FreeleticsAndroidPlugin::class.java)
 
-        val extension = target.freeleticsExtension.extensions.create("legacy", LegacyExtension::class.java)
-
         target.freeleticsAndroidExtension.minSdkVersion(target.appType()?.minSdkVersion(target))
         target.freeleticsAndroidExtension.enableParcelize()
 
-        target.afterEvaluate {
-            target.registerCheckDependencyRulesTasks(
-                allowedProjectTypes = listOf(
-                    ProjectType.DOMAIN_API,
-                    ProjectType.DOMAIN_IMPLEMENTATION,
-                    ProjectType.DOMAIN_TESTING,
-                ),
-                allowedDependencyProjectTypes = listOfNotNull(
-                    ProjectType.CORE_API,
-                    ProjectType.CORE_TESTING,
-                    ProjectType.DOMAIN_API,
-                    ProjectType.DOMAIN_TESTING,
-                    if (target.projectType() == ProjectType.DOMAIN_IMPLEMENTATION) ProjectType.FEATURE_NAV else null,
-                    // TODO remove after migrating domain modules away from legacy
-                    if (extension.allowLegacyDependencies) {
-                        ProjectType.LEGACY
-                    } else {
-                        null
-                    },
-                ),
-            )
-        }
+        target.registerCheckDependencyRulesTasks(
+            allowedProjectTypes = listOf(
+                ProjectType.DOMAIN_API,
+                ProjectType.DOMAIN_IMPLEMENTATION,
+                ProjectType.DOMAIN_TESTING,
+            ),
+            allowedDependencyProjectTypes = listOfNotNull(
+                ProjectType.CORE_API,
+                ProjectType.CORE_TESTING,
+                ProjectType.DOMAIN_API,
+                ProjectType.DOMAIN_TESTING,
+                if (target.projectType() == ProjectType.DOMAIN_IMPLEMENTATION) ProjectType.FEATURE_NAV else null,
+            ),
+        )
 
         target.applyPlatformConstraints()
         target.disableAndroidLibraryTasks()
