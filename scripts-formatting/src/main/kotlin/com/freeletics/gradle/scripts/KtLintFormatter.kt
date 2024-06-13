5f2357ca5cf3055fec474e771e2c35ccc3c5e5c2
Gabriel Ittner <gabriel.ittner@gmail.com>
2024-06-13 12:48:25 +0200

    update API

--- scripts-formatting/src/main/kotlin/com/freeletics/gradle/scripts/KtLintFormatter.kt
+++ scripts-formatting/src/main/kotlin/com/freeletics/gradle/scripts/KtLintFormatter.kt
@@ -5,6 +5,7 @@ import com.pinterest.ktlint.rule.engine.api.EditorConfigDefaults
 import com.pinterest.ktlint.rule.engine.api.KtLintParseException
 import com.pinterest.ktlint.rule.engine.api.KtLintRuleEngine
 import com.pinterest.ktlint.rule.engine.api.LintError
+import com.pinterest.ktlint.rule.engine.core.api.AutocorrectDecision
 import com.pinterest.ktlint.rule.engine.core.api.RuleId
 import com.pinterest.ktlint.ruleset.standard.StandardRuleSetProvider
 import java.nio.file.Path
@@ -24,8 +25,9 @@ internal class KtLintFormatter(
         try {
             // need to use fromFile instead of fromPath because the latter drops the final new line
             val code = Code.fromFile(path.toFile())
-            val formattedContent = engine.format(code) { error, corrected ->
-                check(trySendBlocking(KtLintError(path, error, corrected)).isSuccess)
+            val formattedContent = engine.format(code) { error ->
+                check(trySendBlocking(KtLintError(path, error, error.canBeAutoCorrected)).isSuccess)
+                AutocorrectDecision.ALLOW_AUTOCORRECT
             }
             path.writeText(formattedContent)
         } catch (e: KtLintParseException) {
