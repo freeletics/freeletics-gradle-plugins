3b9b716fb0bb6b9cff7069711a07fb4138bb3c18
Gabriel Ittner <gabriel.ittner@gmail.com>
2024-09-30 10:51:16 +0200

    build health

--- scripts-formatting/scripts-formatting.gradle.kts
+++ scripts-formatting/scripts-formatting.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 dependencies {
     api(libs.kotlin.stdlib)
     api(libs.clikt)
+    api(libs.clikt.core)
     implementation(libs.coroutines)
     implementation(libs.ktlint.rule.engine)
     implementation(libs.ktlint.rule.engine.core)
