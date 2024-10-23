abfd0073f05151288d46848299454ab4c895c871
Gabriel Ittner <gabriel.ittner@gmail.com>
2024-10-23 13:29:26 +0200

    add dependencies on plugins

--- plugin-settings/src/main/kotlin/com/freeletics/gradle/plugin/Utils.kt
+++ plugin-settings/src/main/kotlin/com/freeletics/gradle/plugin/Utils.kt
@@ -1,5 +1,7 @@
 package com.freeletics.gradle.plugin
 
+import nl.littlerobots.vcu.model.VersionCatalog
+import nl.littlerobots.vcu.model.VersionDefinition
 import org.gradle.api.initialization.Settings
 
 internal fun Settings.stringProperty(name: String): String? {
@@ -9,3 +11,17 @@ internal fun Settings.stringProperty(name: String): String? {
 internal fun Settings.booleanProperty(name: String, default: Boolean): Boolean {
     return providers.gradleProperty(name).map { it.toBoolean() }.orElse(default).get()
 }
+
+internal fun VersionDefinition.resolve(versionCatalog: VersionCatalog, settings: Settings): String {
+    return when (this) {
+        is VersionDefinition.Simple -> version
+        is VersionDefinition.Reference -> {
+            settings.stringProperty("${VERSION_OVERRIDE_PREFIX}$ref")
+                ?: versionCatalog.versions.getValue(ref).resolve(versionCatalog, settings)
+        }
+        is VersionDefinition.Condition -> throw IllegalStateException("Conditional version are unsupported")
+        VersionDefinition.Unspecified -> throw IllegalStateException("No version specified")
+    }
+}
+
+internal const val VERSION_OVERRIDE_PREFIX = "fgp.version.override."
