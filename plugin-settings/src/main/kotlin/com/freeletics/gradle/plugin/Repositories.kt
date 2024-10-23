e72622867edc42082cf022c9a8d738f9f4b41386
Gabriel Ittner <gabriel.ittner@gmail.com>
2024-10-23 13:29:25 +0200

    move repository definitions to separate file

---
+++ plugin-settings/src/main/kotlin/com/freeletics/gradle/plugin/Repositories.kt
@@ -0,0 +1,147 @@
+package com.freeletics.gradle.plugin
+
+import org.gradle.api.artifacts.dsl.RepositoryHandler
+import org.gradle.api.artifacts.repositories.InclusiveRepositoryContentDescriptor
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository
+import org.gradle.api.artifacts.repositories.MavenRepositoryContentDescriptor
+import org.gradle.api.credentials.PasswordCredentials
+import org.gradle.api.initialization.Settings
+
+internal fun RepositoryHandler.addMavenCentral() {
+    newRepository("Maven Central", "https://repo.maven.apache.org/maven2/") {
+        releasesOnly()
+    }
+}
+
+internal fun RepositoryHandler.addSonatypeSnapshotRepositories() {
+    newRepository("Sonatype Snapshots", "https://oss.sonatype.org/content/repositories/snapshots/") {
+        snapshotsOnly()
+    }
+
+    newRepository("Sonatype Snapshots S01", "https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+        snapshotsOnly()
+    }
+}
+
+internal fun RepositoryHandler.addKotlinSnapshotRepository() {
+    newExclusiveContentRepository("Kotlin Dev", "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap") {
+        includeVersionByRegex("^org\\.jetbrains\\.kotlin\\..*", ".*", ".*-(dev|release)-.*")
+    }
+}
+
+internal fun RepositoryHandler.addGradlePluginPortalRepository() {
+    newExclusiveContentRepository("Gradle Plugin Portal", "https://plugins.gradle.org/m2") {
+        includeGroupByRegex("^com\\.gradle.*")
+        includeGroupByRegex("^org\\.gradle.*")
+    }
+}
+
+internal fun RepositoryHandler.addGoogleRepository() {
+    newExclusiveContentRepository("Google", "https://dl.google.com/dl/android/maven2/") {
+        includeGroupByRegex("^com\\.android.*")
+        includeGroupByRegex("^android\\..*")
+        // all of AndroidX except for -SNAPSHOT versions
+        includeVersionByRegex("^androidx\\..*", ".*", ".*(?<!-SNAPSHOT)\$")
+        includeGroupByRegex("^com.google.android.*")
+        includeGroup("com.google.firebase")
+        includeGroup("com.google.testing.platform")
+        includeGroup("com.google.android.apps.common.testing.accessibility.framework")
+    }
+}
+
+internal fun RepositoryHandler.addAndroidXSnapshotRepositories(androidXBuildId: String?) {
+    if (androidXBuildId == null) {
+        return
+    }
+
+    newExclusiveContentRepository(
+        "Androidx Snapshots",
+        "https://androidx.dev/snapshots/builds/$androidXBuildId/artifacts/repository/",
+    ) {
+        includeVersionByRegex("^androidx\\..*", ".*", ".*-SNAPSHOT\$")
+    }
+}
+
+internal fun RepositoryHandler.addInternalRepository(settings: Settings) {
+    val internalUrl = settings.stringProperty("fgp.internalArtifacts.url") ?: return
+
+    newExclusiveContentRepository("Internal Artifacts", internalUrl) {
+        includeGroupByRegex("^com\\.freeletics\\.internal.*")
+    }.apply {
+        credentials(PasswordCredentials::class.java)
+    }
+}
+
+// TODO remove after https://youtrack.jetbrains.com/issue/KT-68533
+internal fun RepositoryHandler.addKotlinJsRepositories() {
+    exclusiveContent { content ->
+        content.forRepository {
+            ivy { ivy ->
+                ivy.name = "Node Distributions"
+                ivy.setUrl("https://nodejs.org/dist/")
+                ivy.patternLayout { it.artifact("v[revision]/[artifact](-v[revision]-[classifier]).[ext]") }
+                ivy.metadataSources { it.artifact() }
+                ivy.content { it.includeModule("org.nodejs", "node") }
+            }
+        }
+
+        content.filter { it.includeGroup("org.nodejs") }
+    }
+
+    exclusiveContent { content ->
+        content.forRepository {
+            ivy { ivy ->
+                ivy.name = "Yarn Distributions"
+                ivy.setUrl("https://github.com/yarnpkg/yarn/releases/download")
+                ivy.patternLayout { it.artifact("v[revision]/[artifact](-v[revision]).[ext]") }
+                ivy.metadataSources { it.artifact() }
+                ivy.content { it.includeModule("com.yarnpkg", "yarn") }
+            }
+        }
+
+        content.filter { it.includeGroup("com.yarnpkg") }
+    }
+
+    exclusiveContent { content ->
+        content.forRepository {
+            ivy { ivy ->
+                ivy.name = "Binaryen Distributions"
+                ivy.setUrl("https://github.com/WebAssembly/binaryen/releases/download")
+                ivy.patternLayout {
+                    it.artifact("version_[revision]/[module]-version_[revision]-[classifier].[ext]")
+                }
+                ivy.metadataSources { it.artifact() }
+                ivy.content { it.includeModule("com.github.webassembly", "binaryen") }
+            }
+        }
+
+        content.filter { it.includeGroup("com.github.webassembly") }
+    }
+}
+
+private fun RepositoryHandler.newRepository(
+    name: String,
+    url: String,
+    content: MavenRepositoryContentDescriptor.() -> Unit = {},
+): MavenArtifactRepository {
+    return maven {
+        it.name = name
+        it.setUrl(url)
+        it.mavenContent(content)
+    }
+}
+
+private fun RepositoryHandler.newExclusiveContentRepository(
+    name: String,
+    url: String,
+    content: InclusiveRepositoryContentDescriptor.() -> Unit,
+): MavenArtifactRepository {
+    val repository = newRepository(name, url)
+    exclusiveContent {
+        it.forRepository { repository }
+        it.filter {
+            it.content()
+        }
+    }
+    return repository
+}
--- plugin-settings/src/main/kotlin/com/freeletics/gradle/plugin/SettingsPlugin.kt
+++ plugin-settings/src/main/kotlin/com/freeletics/gradle/plugin/SettingsPlugin.kt
@@ -3,8 +3,6 @@ package com.freeletics.gradle.plugin
 import com.gradle.develocity.agent.gradle.DevelocityConfiguration
 import com.gradle.develocity.agent.gradle.DevelocityPlugin
 import org.gradle.api.Plugin
-import org.gradle.api.artifacts.dsl.RepositoryHandler
-import org.gradle.api.credentials.PasswordCredentials
 import org.gradle.api.initialization.Settings
 import org.gradle.api.initialization.resolve.RepositoriesMode
 import org.gradle.caching.http.HttpBuildCache
@@ -23,9 +21,9 @@ public abstract class SettingsPlugin : Plugin<Settings> {
             it.buildScan { scan ->
                 scan.termsOfUseUrl.set("https://gradle.com/terms-of-service")
                 scan.termsOfUseAgree.set("yes")
-                scan.capture {
-                    it.buildLogging.set(false)
-                    it.testLogging.set(false)
+                scan.capture { capture ->
+                    capture.buildLogging.set(false)
+                    capture.testLogging.set(false)
                 }
                 scan.publishing.onlyIf { false }
             }
@@ -51,58 +49,12 @@ public abstract class SettingsPlugin : Plugin<Settings> {
             management.repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 
             @Suppress("UnstableApiUsage")
-            management.repositories { handler ->
-                val internalUrl = target.stringProperty("fgp.internalArtifacts.url")
-                if (internalUrl != null) {
-                    handler.exclusiveContent { content ->
-                        content.forRepository {
-                            handler.maven {
-                                it.name = "internalArtifacts"
-                                it.setUrl(internalUrl)
-                                it.credentials(PasswordCredentials::class.java)
-                            }
-                        }
-
-                        content.filter {
-                            it.includeGroupByRegex("^com\\.freeletics\\.internal.*")
-                        }
-                    }
-                }
-
-                handler.exclusiveContent { content ->
-                    content.forRepository { handler.google() }
-
-                    content.filter {
-                        it.includeGroupByRegex("^com\\.android.*")
-                        it.includeGroupByRegex("^android\\..*")
-                        // all of AndroidX except for androidx.compose.compiler and -SNAPSHOT versions
-                        it.includeVersionByRegex("^androidx\\..*(?<!compose\\.compiler)\$", ".*", ".*(?<!-SNAPSHOT)\$")
-                        // androidx.compose.compiler but not dev versions because they come from a different repository
-                        it.includeVersionByRegex("^androidx.compose.compiler\$", ".*", "^((?!-dev-k).)*\$")
-
-                        it.includeGroupByRegex("^com.google.android.*")
-                        it.includeGroup("com.google.firebase")
-                        it.includeGroup("com.google.testing.platform")
-                        it.includeGroup("com.google.android.apps.common.testing.accessibility.framework")
-                    }
-                }
-
-                handler.exclusiveContent { content ->
-                    content.forRepository { handler.gradlePluginPortal() }
-
-                    content.filter {
-                        it.includeGroupByRegex("^com.gradle.*")
-                        it.includeGroupByRegex("^org.gradle.*")
-                    }
-                }
-
-                handler.mavenCentral {
-                    it.mavenContent { content ->
-                        content.releasesOnly()
-                    }
-                }
-
-                handler.jsRepositoryWorkaround()
+            management.repositories.apply {
+                addMavenCentral()
+                addGoogleRepository()
+                addGradlePluginPortalRepository()
+                addKotlinJsRepositories()
+                addInternalRepository(target)
             }
 
             val prefix = "fgp.version.override."
@@ -137,61 +89,6 @@ public abstract class SettingsPlugin : Plugin<Settings> {
         val autoDiscover = target.booleanProperty("fgp.discoverProjects.automatically", true)
         if (autoDiscover) {
             extension.discoverProjects(listOf("gradle", "gradle.kts"))
-        }
-    }
-
-    private fun Settings.stringProperty(name: String): String? {
-        return providers.gradleProperty(name).orNull
-    }
-
-    private fun Settings.booleanProperty(name: String, default: Boolean): Boolean {
-        return providers.gradleProperty(name).map { it.toBoolean() }.orElse(default).get()
-    }
-
-    // TODO remove after https://youtrack.jetbrains.com/issue/KT-68533
-    private fun RepositoryHandler.jsRepositoryWorkaround() {
-        exclusiveContent { content ->
-            content.forRepository {
-                ivy { ivy ->
-                    ivy.name = "Node Distributions"
-                    ivy.setUrl("https://nodejs.org/dist/")
-                    ivy.patternLayout { it.artifact("v[revision]/[artifact](-v[revision]-[classifier]).[ext]") }
-                    ivy.metadataSources { it.artifact() }
-                    ivy.content { it.includeModule("org.nodejs", "node") }
-                }
-            }
-
-            content.filter { it.includeGroup("org.nodejs") }
-        }
-
-        exclusiveContent { content ->
-            content.forRepository {
-                ivy { ivy ->
-                    ivy.name = "Yarn Distributions"
-                    ivy.setUrl("https://github.com/yarnpkg/yarn/releases/download")
-                    ivy.patternLayout { it.artifact("v[revision]/[artifact](-v[revision]).[ext]") }
-                    ivy.metadataSources { it.artifact() }
-                    ivy.content { it.includeModule("com.yarnpkg", "yarn") }
-                }
-            }
-
-            content.filter { it.includeGroup("com.yarnpkg") }
-        }
-
-        exclusiveContent { content ->
-            content.forRepository {
-                ivy { ivy ->
-                    ivy.name = "Binaryen Distributions"
-                    ivy.setUrl("https://github.com/WebAssembly/binaryen/releases/download")
-                    ivy.patternLayout {
-                        it.artifact("version_[revision]/[module]-version_[revision]-[classifier].[ext]")
-                    }
-                    ivy.metadataSources { it.artifact() }
-                    ivy.content { it.includeModule("com.github.webassembly", "binaryen") }
-                }
-            }
-
-            content.filter { it.includeGroup("com.github.webassembly") }
         }
     }
 }
