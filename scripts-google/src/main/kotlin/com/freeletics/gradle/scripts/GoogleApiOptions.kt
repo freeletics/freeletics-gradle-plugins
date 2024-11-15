1b6e83da87cfae8742c63d355b41536a2087adef
Gabriel Ittner <gabriel.ittner@gmail.com>
2024-11-15 11:35:48 +0100

    add base64 encoding

--- scripts-google/src/main/kotlin/com/freeletics/gradle/scripts/GoogleApiOptions.kt
+++ scripts-google/src/main/kotlin/com/freeletics/gradle/scripts/GoogleApiOptions.kt
@@ -1,13 +1,15 @@
 package com.freeletics.gradle.scripts
 
 import com.github.ajalt.clikt.parameters.groups.OptionGroup
+import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import java.util.Base64
 
 public class GoogleApiOptions : OptionGroup("Google API options") {
     public val jsonKeyData: String by option(
         "--json-key-data",
         envvar = "GOOGLE_PLAY_JSON_KEY_DATA",
-        help = "The json key for a Google Play service account",
-    ).required()
+        help = "The json key for a Google Play service account. Needs to be provided in base64 encoded form.",
+    ).convert { Base64.getDecoder().decode(it).toString(Charsets.UTF_8) }.required()
 }
