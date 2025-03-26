8ef7080e060484f22e3a35c3ed9f37a7fb46940c
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-03-26 16:44:20 +0100

    replace deprecated property

--- plugins/src/main/kotlin/com/freeletics/gradle/plugin/RootPlugin.kt
+++ plugins/src/main/kotlin/com/freeletics/gradle/plugin/RootPlugin.kt
@@ -6,6 +6,7 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.buildconfiguration.tasks.UpdateDaemonJvm
 import org.gradle.internal.jvm.inspection.JvmVendor
+import org.gradle.jvm.toolchain.JvmVendorSpec
 
 public abstract class RootPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -19,7 +20,7 @@ public abstract class RootPlugin : Plugin<Project> {
     @Suppress("UnstableApiUsage")
     private fun configureDaemonToolchainTask(target: Project) {
         target.tasks.withType(UpdateDaemonJvm::class.java).configureEach {
-            it.jvmVendor.set(JvmVendor.KnownJvmVendor.AZUL.name)
+            it.vendor.set(JvmVendorSpec.AZUL)
         }
     }
 
