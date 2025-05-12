0bf10ef32d13c7687407289c1ce48aa422f9a256
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-05-12 21:04:46 +0200

    prepare for Kotlin 2.2.0

--- plugins/src/main/kotlin/com/freeletics/gradle/plugin/FreeleticsBasePlugin.kt
+++ plugins/src/main/kotlin/com/freeletics/gradle/plugin/FreeleticsBasePlugin.kt
@@ -14,6 +14,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.jvm.tasks.Jar
 import org.gradle.jvm.toolchain.JvmVendorSpec
+import org.jetbrains.kotlin.gradle.dsl.JvmDefaultMode
 import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
@@ -77,13 +78,8 @@ public abstract class FreeleticsBasePlugin : Plugin<Project> {
                 extraWarnings.set(booleanProperty("fgp.kotlin.extraWarnings", true))
                 allWarningsAsErrors.set(booleanProperty("fgp.kotlin.warningsAsErrors", true))
                 if (booleanProperty("fgp.kotlin.suppressDeprecationWarnings", false).get()) {
-                    if (version < KotlinVersion.KOTLIN_2_2) {
-                        freeCompilerArgs.add("-Xsuppress-warning=DEPRECATION")
-                        freeCompilerArgs.add("-Xsuppress-warning=OVERRIDE_DEPRECATION")
-                    } else {
-                        freeCompilerArgs.add("-Xwarning-level=DEPRECATION:disabled")
-                        freeCompilerArgs.add("-Xwarning-level=OVERRIDE_DEPRECATION:disabled")
-                    }
+                    freeCompilerArgs.add("-Xwarning-level=DEPRECATION:disabled")
+                    freeCompilerArgs.add("-Xwarning-level=OVERRIDE_DEPRECATION:disabled")
                 }
 
                 // In this mode, some deprecations and bug-fixes for unstable code take effect immediately.
@@ -94,42 +90,13 @@ public abstract class FreeleticsBasePlugin : Plugin<Project> {
                     "-Xannotation-default-target=param-property",
                     // https://kotlinlang.org/docs/whatsnew2020.html#data-class-copy-function-to-have-the-same-visibility-as-constructor
                     "-Xconsistent-data-class-copy-visibility",
+                    // Enable context parameters (2.2.0 beta feature)
+                    "-Xcontext-parameters",
                 )
-
-                if (version >= KotlinVersion.KOTLIN_2_1 && version < KotlinVersion.KOTLIN_2_2) {
-                    freeCompilerArgs.addAll(
-                        // Kotlin 2.1 experimental language features
-                        "-Xwhen-guards",
-                        "-Xnon-local-break-continue",
-                        "-Xmulti-dollar-interpolation",
-                    )
-                }
-
-                if (version < KotlinVersion.KOTLIN_2_2) {
-                    // Support inferring type arguments based on only self upper bounds of the corresponding type parameters
-                    // https://kotlinlang.org/docs/whatsnew1530.html#improvements-to-type-inference-for-recursive-generic-types
-                    freeCompilerArgs.add("-Xself-upper-bound-inference")
-                }
-
-                if (version >= KotlinVersion.KOTLIN_2_2) {
-                    freeCompilerArgs.addAll(
-                        // Enable context parameters (2.2.0 beta feature)
-                        "-Xcontext-parameters",
-                    )
-                }
 
                 if (this is KotlinJvmCompilerOptions) {
                     jvmTarget.set(project.jvmTarget)
-
-                    if (version >= KotlinVersion.KOTLIN_2_2) {
-                        // https://youtrack.jetbrains.com/issue/KT-73007
-                        freeCompilerArgs.addAll("-jvm-default=no-compatibility")
-                    } else {
-                        freeCompilerArgs.addAll(
-                            // https://blog.jetbrains.com/kotlin/2020/07/kotlin-1-4-m3-generating-default-methods-in-interfaces/
-                            "-Xjvm-default=all",
-                        )
-                    }
+                    jvmDefault.set(JvmDefaultMode.NO_COMPATIBILITY)
 
                     freeCompilerArgs.addAll(
                         // https://youtrack.jetbrains.com/issue/KT-22292
@@ -140,14 +107,9 @@ public abstract class FreeleticsBasePlugin : Plugin<Project> {
                         "-Xjspecify-annotations=strict",
                         // Enhance not null annotated type parameter's types to definitely not null types (@NotNull T => T & Any)
                         "-Xenhance-type-parameter-types-to-def-not-null",
+                        // https://kotlinlang.org/docs/whatsnew-eap.html#support-for-reading-and-writing-annotations-in-kotlin-metadata
+                        "-Xannotations-in-metadata"
                     )
-
-                    if (version >= KotlinVersion.KOTLIN_2_2) {
-                        freeCompilerArgs.addAll(
-                            // https://kotlinlang.org/docs/whatsnew-eap.html#support-for-reading-and-writing-annotations-in-kotlin-metadata
-                            "-Xannotations-in-metadata",
-                        )
-                    }
 
                     if (!isAndroid) {
                         freeCompilerArgs.add("-Xjdk-release=${project.javaTarget}")
