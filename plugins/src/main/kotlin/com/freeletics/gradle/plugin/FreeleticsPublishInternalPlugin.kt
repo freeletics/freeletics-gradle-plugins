d31e1e5dd94bd6daba7ffee6542d88328ad4bff8
Gabriel Ittner <gabriel@freeletics.com>
2025-05-16 15:22:07 +0200

    stop disabling iOS publishing (#588)
    * stop disabling iOS publishing
    
    * ktlint

--- plugins/src/main/kotlin/com/freeletics/gradle/plugin/FreeleticsPublishInternalPlugin.kt
+++ plugins/src/main/kotlin/com/freeletics/gradle/plugin/FreeleticsPublishInternalPlugin.kt
@@ -6,7 +6,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.credentials.PasswordCredentials
 import org.gradle.api.publish.PublishingExtension
-import org.gradle.api.publish.maven.tasks.AbstractPublishToMaven
 
 public abstract class FreeleticsPublishInternalPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -14,7 +13,6 @@ public abstract class FreeleticsPublishInternalPlugin : Plugin<Project> {
 
         target.addInternalRepo()
         target.addLocalRepo()
-        target.disablePublishingIosArtifacts()
         target.configurePom(includeLicense = false)
     }
 
@@ -43,14 +41,6 @@ public abstract class FreeleticsPublishInternalPlugin : Plugin<Project> {
                         it.setUrl(localPath)
                     }
                 }
-            }
-        }
-    }
-
-    private fun Project.disablePublishingIosArtifacts() {
-        tasks.withType(AbstractPublishToMaven::class.java).configureEach {
-            if (it.name.contains("ios", ignoreCase = true)) {
-                it.onlyIf { false }
             }
         }
     }
