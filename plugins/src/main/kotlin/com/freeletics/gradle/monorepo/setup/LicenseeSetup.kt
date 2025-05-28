28eda41c613e0d86fa9bb4d701928cc58c3e2853
Gabriel Ittner <gabriel@freeletics.com>
2025-05-28 09:10:20 +0200

    add task to verify licenses haven't changed (#599)

--- plugins/src/main/kotlin/com/freeletics/gradle/monorepo/setup/LicenseeSetup.kt
+++ plugins/src/main/kotlin/com/freeletics/gradle/monorepo/setup/LicenseeSetup.kt
@@ -3,12 +3,14 @@ package com.freeletics.gradle.monorepo.setup
 import app.cash.licensee.LicenseeExtension
 import app.cash.licensee.SpdxId
 import com.freeletics.gradle.monorepo.tasks.UpdateLicensesTask.Companion.registerUpdateLicensesTask
+import com.freeletics.gradle.monorepo.tasks.VerifyLicensesTask.Companion.registerVerifyLicensesTask
 import org.gradle.api.Project
 
 internal fun Project.configureLicensee() {
     plugins.apply("app.cash.licensee")
 
     registerUpdateLicensesTask()
+    registerVerifyLicensesTask()
 
     extensions.configure(LicenseeExtension::class.java) {
         it.allow(SpdxId.Apache_20)
