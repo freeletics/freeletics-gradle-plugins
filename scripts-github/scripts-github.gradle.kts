d1a544f5bdd88c386ec571d8bfb5efcb91e1a9c4
jalalawqati <jalalawqati@icloud.com>
2025-06-13 14:37:26 +0200

    add github options

---
+++ scripts-github/scripts-github.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.freeletics.gradle.jvm")
+    id("com.freeletics.gradle.publish.oss")
+}
+
+dependencies {
+    api(libs.kotlin.stdlib)
+    api(libs.clikt)
+    api(libs.clikt.core)
+}
