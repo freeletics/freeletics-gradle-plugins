c88b2ed302b83c2b2ed9252c32fa9691966b779d
Gabriel Ittner <gabriel@freeletics.com>
2025-07-16 13:31:53 +0200

    addCommonTargets: replace androidNativeTargets with limitToComposeTargets (#670)

--- plugins/src/main/kotlin/com/freeletics/gradle/plugin/FreeleticsMultiplatformExtension.kt
+++ plugins/src/main/kotlin/com/freeletics/gradle/plugin/FreeleticsMultiplatformExtension.kt
@@ -129,7 +129,7 @@ public abstract class FreeleticsMultiplatformExtension(private val project: Proj
         }
     }
 
-    public fun addCommonTargets(androidNativeTargets: Boolean = true) {
+    public fun addCommonTargets(limitToComposeTargets: Boolean = false) {
         project.kotlinMultiplatform {
             jvm()
 
@@ -142,9 +142,11 @@ public abstract class FreeleticsMultiplatformExtension(private val project: Proj
                 nodejs()
             }
 
-            @OptIn(ExperimentalWasmDsl::class)
-            wasmWasi {
-                nodejs()
+            if (!limitToComposeTargets) {
+                @OptIn(ExperimentalWasmDsl::class)
+                wasmWasi {
+                    nodejs()
+                }
             }
 
             linuxX64()
@@ -165,11 +167,14 @@ public abstract class FreeleticsMultiplatformExtension(private val project: Proj
 
             watchosArm32()
             watchosArm64()
-            watchosDeviceArm64()
+            // TODO remove check when Compose 1.9.0 is stable
+            if (!limitToComposeTargets) {
+                watchosDeviceArm64()
+            }
             watchosX64()
             watchosSimulatorArm64()
 
-            if (androidNativeTargets) {
+            if (!limitToComposeTargets) {
                 androidNativeArm32()
                 androidNativeArm64()
                 androidNativeX86()
